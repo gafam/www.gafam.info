f227f12fbdef0479e985e96e4185d4968538a70b
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-01-01 20:09:23 +0100

    Add Smooth scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,31 @@ $(document).ready(function() {
     // Initialize video player
     initialize_video_player();
 
+    // Smooth scrolling
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { 
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
+        });
 });
 
 function initialize_github_embed() {
