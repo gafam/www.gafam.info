3fc3bd538e8a12ddadd084ef0b96869fdb1f4454
The GAFAM tech collective <tech@gafam.info>
2022-01-01 21:09:16 +0100

    Move "smooth scrolling" feature into separate function

--- src/js/main.js
+++ src/js/main.js
@@ -21,30 +21,8 @@ $(document).ready(function() {
     initialize_video_player();
 
     // Smooth scrolling
-    $('a[href*="#"]')
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function (event) {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function () {
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) { 
-                            return false;
-                        } else {
-                            $target.attr('tabindex', '-1');
-                            $target.focus();
-                        };
-                    });
-                }
-            }
-        });
+    initialize_smooth_scrolling();
+
 });
 
 function initialize_github_embed() {
@@ -134,4 +112,31 @@ function initialize_video_player() {
         }
     });
 
+}
+
+function initialize_smooth_scrolling() {
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
+        });
 }
